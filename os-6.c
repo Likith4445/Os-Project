c16e8192eb0615c5d7f5be9dfac0130a5285ba6b
Likith4445 <44252409+Likith4445@users.noreply.github.com>
2019-04-24 04:18:39 +0530

    os-6.c

---
+++ os-6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int bt[20],p[20],wt[20],t[20],i,j,n,total=0,pos,temp;
+    float avg_wt,avg_t;
+    printf("Enter number of students:");
+    scanf("%d",&n);
+}
