ae7dc10fdeab96a4afc16f7b1d3c0f04d8acfc49
Likith4445 <44252409+Likith4445@users.noreply.github.com>
2019-04-24 04:24:26 +0530

    Create os-2.c

---
+++ os-2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int bt[20],p[20],wt[20],t[20],i,j,n,total=0,pos,temp;
+    float avg_wt,avg_t;
+    printf("Enter number of students:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter no. of gifts taken by each student :\n");
+    for(i=0;i<n;i++)
+    {
+        printf("p%d:",i+1);
+        scanf("%d",&bt[i]);
+        p[i]=i+1;           //contains process numbers
+    }
+ 
+    //sorting burst time in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[j]>bt[pos])
+                pos=j;
+        }
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+ 
+    wt[0]=0;            //waiting time for first process will be zero
+ 
+    //calculate waiting time
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+ 
+        total+=wt[i];
+    }
+ 
