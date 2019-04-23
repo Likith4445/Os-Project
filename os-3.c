6442ceac1bc87652a2e6c7ac67c582538a058154
Likith4445 <44252409+Likith4445@users.noreply.github.com>
2019-04-24 04:23:36 +0530

    Create os-3.c

---
+++ os-3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+ 
+int main()
+{
+    int bt[20],p[20],wt[20],t[20],i,j,n,total=0,pos,temp;
+    float avg_wt,avg_t;
+    printf("Enter number of students:");
+    scanf("%d",&n);
+ 
+    printf("\nEnter no. of gifts taken by each student :\n");
+    for(i=0;i<n;i++)
+    {
+        printf("p%d:",i+1);
+        scanf("%d",&bt[i]);
+        p[i]=i+1;           //contains process numbers
+    }
+ 
+    //sorting burst time in ascending order using selection sort
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[j]>bt[pos])
+                pos=j;
+        }
+ 
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+ 
+        temp=p[i];
+        p[i]=p[pos];
+        p[pos]=temp;
+    }
+}
